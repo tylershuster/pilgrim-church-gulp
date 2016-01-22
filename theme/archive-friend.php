20c17d35674ad5725826edcdc1e25374052dde8f
Tyler Shuster <tyler@shuster.house>
2016-01-21 16:12:13 -0800

    tweaks

--- theme/archive-friend.php
+++ theme/archive-friend.php
@@ -26,40 +26,126 @@ function last_name_sort( $a, $b ) {
 
 uasort( $wp_query->posts, 'last_name_sort' );
 
-get_header();
 
-if( is_user_logged_in() ):
+get_header(); ?>
 
-	echo "<p>You can find more about each member by clicking their names.</p>";
+<?php if( is_user_logged_in() ): ?>
 
-	if( $wp_query->have_posts() ):
+	<p>You can find more about each member by clicking their names.</p>
 
-		echo "<ul id='directory'>";
+	<input type="text" name="filter_members" value="" placeholder="Type here to search">
 
-		foreach( $wp_query->posts as $post ): setup_postdata( $post );
+	<?php if( $wp_query->have_posts() ): ?>
 
-		echo "<li><a href='" . get_the_permalink() . "'>";
+		<ul id='directory'>
 
-			if( has_post_thumbnail() ):
-				the_post_thumbnail( 'directory_thumbnail' );
-			else:
-				echo "<img src='" . get_stylesheet_directory_uri() . "/assets/img/heart.png' />";
-			endif;
+		<?php foreach( $wp_query->posts as $post ): setup_postdata( $post ); ?>
 
-			the_title();
-		echo "</a></li>";
+		<li><a href='<?php echo get_the_permalink(); ?>'>
 
-		endforeach;
+			<?php if( has_post_thumbnail() ): ?>
 
-		echo "</ul>";
+				<?php the_post_thumbnail( 'directory_thumbnail' ); ?>
 
-	endif;
+			<?php else: ?>
 
-else:
+				<img src='<?php echo get_stylesheet_directory_uri(); ?>/assets/img/heart.png' />
 
-	echo "Sorry, you must be logged into access this page";
+			<?php endif; ?>
 
-endif;
+			<?php the_title(); ?>
 
-get_footer();
- ?>
+		</a></li>
+
+		<?php endforeach; ?>
+
+		</ul>
+
+	<?php endif; ?>
+
+
+
+	<script type="application/javascript">
+
+	jQuery(document).ready(function($) {
+
+		$('input[name=filter_members]').fastLiveFilter('#directory');
+
+	});
+
+	</script>
+
+	<script type="text/javascript">
+	/**
+* fastLiveFilter jQuery plugin 1.0.3
+*
+* Copyright (c) 2011, Anthony Bush
+* License: <http://www.opensource.org/licenses/bsd-license.php>
+* Project Website: http://anthonybush.com/projects/jquery_fast_live_filter/
+**/
+
+jQuery.fn.fastLiveFilter = function(list, options) {
+// Options: input, list, timeout, callback
+options = options || {};
+list = jQuery(list);
+var input = this;
+var lastFilter = '';
+var timeout = options.timeout || 0;
+var callback = options.callback || function() {};
+
+var keyTimeout;
+
+// NOTE: because we cache lis & len here, users would need to re-init the plugin
+// if they modify the list in the DOM later.  This doesn't give us that much speed
+// boost, so perhaps it's not worth putting it here.
+var lis = list.children();
+var len = lis.length;
+var oldDisplay = len > 0 ? lis[0].style.display : "block";
+callback(len); // do a one-time callback on initialization to make sure everything's in sync
+
+input.change(function() {
+	// var startTime = new Date().getTime();
+	var filter = input.val().toLowerCase();
+	var li, innerText;
+	var numShown = 0;
+	for (var i = 0; i < len; i++) {
+		li = lis[i];
+		innerText = !options.selector ?
+			(li.textContent || li.innerText || "") :
+			$(li).find(options.selector).text();
+
+		if (innerText.toLowerCase().indexOf(filter) >= 0) {
+			if (li.style.display == "none") {
+				li.style.display = oldDisplay;
+			}
+			numShown++;
+		} else {
+			if (li.style.display != "none") {
+				li.style.display = "none";
+			}
+		}
+	}
+	callback(numShown);
+	// var endTime = new Date().getTime();
+	// console.log('Search for ' + filter + ' took: ' + (endTime - startTime) + ' (' + numShown + ' results)');
+	return false;
+}).keydown(function() {
+	clearTimeout(keyTimeout);
+	keyTimeout = setTimeout(function() {
+		if( input.val() === lastFilter ) return;
+		lastFilter = input.val();
+		input.change();
+	}, timeout);
+});
+return this; // maintain jQuery chainability
+}
+
+	</script>
+
+<?php else: ?>
+
+	Sorry, you must be logged into access this page. Please click <a href="<?php echo get_bloginfo('url'); ?>/login">here</a> to log in or register.
+
+<?php endif; ?>
+
+<?php get_footer(); ?>
--- theme/front-page.php
+++ theme/front-page.php
@@ -5,17 +5,37 @@ if( have_posts() ): while( have_posts() ): the_post();
 
 	the_content();
 
-
-	echo "<div class='home__widgets'>";
-
-		echo do_shortcode('[latest_sermons]');
-
-		echo do_shortcode('[upcoming_events]');
-
-	echo "</div>";
-
-
 endwhile; endif;
+
+
+echo "<div class='home__widgets'>";
+
+echo do_shortcode('[latest_sermons]');
+
+echo do_shortcode('[upcoming_events]');
+
+echo "</div>";
+
+//
+// $json = file_get_contents("http://calapi.inadiutorium.cz/api/v0/en/calendars/default/today");
+//
+// $json = json_decode( $json );
+
+?>
+
+<style media="screen">
+	.home__liturgical-info {
+		border-color: <?php //echo $json->celebrations[0]->colour; ?>
+	}
+</style>
+
+<?php
+
+// echo "<div class='home__liturgical-info'>";
+//
+// 	echo "We are in the " . ordinal( $json->season_week ) . " week of " . $json->season . "!";
+//
+// echo "</div>";
 
 get_footer();
  ?>
